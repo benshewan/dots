01861414e21b4b5d0934f53d97e4d93403123624
Ben Shewan <benbshewan@gmail.com>
2023-07-23 11:59:06 -0300

    almost got nody-greeter working

--- nody-greeter/default.nix
+++ nody-greeter/default.nix
@@ -1,20 +1,49 @@
-{ stdenv,
-  fetchFromGithub
-  nodejs_16,
-  python311,
-  ... 
-  }:
+{ stdenv
+, fetchFromGitHub
+, buildNpmPackage
+, nodejs_16
+, electron_16
+, pkg-config
+, cairo
+, gobject-introspection
+, python3
+, ...
+}:
 
-stdenv.mkDerivation {
-  name = "nody-greeter";
-  version = "v1.5.2";
+let
 
-  src = fetchFromGithub (finalAttrs: {
+  buildNpmPackage' = buildNpmPackage.override { nodejs = nodejs_16; };
+
+  version = "1.5.2";
+  src = fetchFromGitHub {
     owner = "JezerM";
     repo = "nody-greeter";
-    rev = "refs/tags/${finalAttrs.version}";
-    sha256 = "126i0aw3gp1k9lgq1r8m9c6syqq8pz42hw9d394pvjd3x5q19r7l";
-  });
+    rev = "refs/tags/${version}";
+    sha256 = "w4aEjqBVjjZMH1MkogJ8Shxbm6vE9gXxsypLeiYKxm8=";
+    fetchSubmodules = true;
+  };
 
-  buildInputs = [ nodejs_16 python311 ];
-}
+in
+buildNpmPackage' {
+    pname = "nody-greeter";
+    inherit src version;
+
+    makeCacheWritable = true;
+    npmDepsHash = "sha256-wcFNeeCnxc4jh9ybpWZyFKnCH3oGgDl+Wv7J0qX5/GI=";
+
+    buildInputs = [ nodejs_16 python3 electron_16 pkg-config cairo gobject-introspection ];
+    nativeBuildInputs = [ pkg-config cairo gobject-introspection ];
+
+    ELECTRON_SKIP_BINARY_DOWNLOAD = "1";
+    ELECTRON_OVERRIDE_DIST_PATH = "${electron_16}/bin/";
+
+    buildPhase = ''
+      ln -sf ${electron_16}/lib/electron ./node_modules/electron/dist
+      npm run rebuild
+      npm run build
+    '';
+
+    installPhase = ''
+      node make install --DEST_DIR $out --PREFIX /
+    '';
+  }
--- nody-greeter/flake.nix
+++ nody-greeter/flake.nix
@@ -2,21 +2,21 @@
   description = "This is a LightDM Greeter made with Electron.js and node-gtk that allows to create web based themes with HTMl, CSS and JavaScript.";
 
   inputs = {
-    nixpkgs = {
-      url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    };
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
   outputs = { self, nixpkgs }:
     let
-      pkgs = import nixpkgs { inherit system; };
+      pkgs = import nixpkgs {
+        inherit system;
+        config.permittedInsecurePackages = [ "nodejs-16.20.1" "electron-16.2.3" ];
+      };
       system = "x86_64-linux";
     in
     {
-
       packages.${system} = {
-        nody-greeter = pkgs.callPackage ./. {};
+        nody-greeter = pkgs.callPackage ./. { };
         default = self.packages.${system}.nody-greeter;
-
+      };
     };
 }
