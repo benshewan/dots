96a38fa883362b443234247ce1b30ee832229560
benshewan <benbshewan@gmail.com>
2023-09-04 15:06:44 -0300

    changed var structure

--- users/shared/desktop-enviroments/hyprland/waybar.nix
+++ users/shared/desktop-enviroments/hyprland/waybar.nix
@@ -1,4 +1,6 @@
-{ custom ? rec {
+{ ... }:
+let
+  custom = {
     font = "RobotoMono Nerd Font";
     fontsize = "12";
     primary_accent = "cba6f7";
@@ -7,26 +9,35 @@
     background = "11111B";
     opacity = ".85";
     cursor = "Numix-Cursor";
-    palette = {
-      primary_accent_hex = "cba6f7";
-      secondary_accent_hex = "89b4fa";
-      tertiary_accent_hex = "f5f5f5";
-      primary_background_hex = "11111B";
-      secondary_background_hex = "1b1b2b";
-      tertiary_background_hex = "25253a";
-
-      primary_accent_rgba = "rgba(203,166,247,${opacity})";
-      secondary_accent_rgba = "rgba(137,180,250,${opacity})";
-      tertiary_accent_rgba = "rgba(245,245,245,${opacity})";
-      primary_background_rgba = "rgba(17,17,27,${opacity})";
-      secondary_background_rgba = "rgba(27,27,43,${opacity})";
-      tertiary_background_rgba = "rgba(37,37,58,${opacity})";
-
+    palette = rec {
+      font = "RobotoMono Nerd Font";
+      fontsize = "12";
+      primary_accent = "cba6f7";
+      secondary_accent = "89b4fa";
+      tertiary_accent = "f5f5f5";
+      background = "11111B";
       opacity = ".85";
+      cursor = "Numix-Cursor";
+      palette = {
+        primary_accent_hex = "cba6f7";
+        secondary_accent_hex = "89b4fa";
+        tertiary_accent_hex = "f5f5f5";
+        primary_background_hex = "11111B";
+        secondary_background_hex = "1b1b2b";
+        tertiary_background_hex = "25253a";
+
+        primary_accent_rgba = "rgba(203,166,247,${opacity})";
+        secondary_accent_rgba = "rgba(137,180,250,${opacity})";
+        tertiary_accent_rgba = "rgba(245,245,245,${opacity})";
+        primary_background_rgba = "rgba(17,17,27,${opacity})";
+        secondary_background_rgba = "rgba(27,27,43,${opacity})";
+        tertiary_background_rgba = "rgba(37,37,58,${opacity})";
+
+        opacity = ".85";
+      };
     };
-  }
-, ...
-}:
+  };
+in
 {
   programs.waybar = {
     enable = true;
