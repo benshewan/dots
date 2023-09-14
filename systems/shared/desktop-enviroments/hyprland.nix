7aeaeb4534671ba4861651bf8291fc2f836f4b22
benshewan <benbshewan@gmail.com>
2023-09-14 18:46:09 -0300

    commented out some stuff from hyprland config

--- systems/shared/desktop-enviroments/hyprland.nix
+++ systems/shared/desktop-enviroments/hyprland.nix
@@ -1,56 +1,56 @@
 { pkgs, config, lib, ... }:
-let
-  # exec "dbus-update-activation-environment --systemd DISPLAY WAYLAND_DISPLAY SWAYSOCK XDG_CURRENT_DESKTOP"
-  # input "type:touchpad" {
-  #   tap enabled
-  # }
-  # seat seat0 xcursor_theme Bibata-Modern-Classic 24
+# let
+# exec "dbus-update-activation-environment --systemd DISPLAY WAYLAND_DISPLAY SWAYSOCK XDG_CURRENT_DESKTOP"
+# input "type:touchpad" {
+#   tap enabled
+# }
+# seat seat0 xcursor_theme Bibata-Modern-Classic 24
 
-  # xwayland disable
+# xwayland disable
 
-  # bindsym XF86MonBrightnessUp exec light -A 5
-  # bindsym XF86MonBrightnessDown exec light -U 5
-  # bindsym Print exec ${lib.getExe pkgs.grim} /tmp/regreet.png
-  # bindsym Mod4+shift+e exec swaynag \
-  #   -t warning \
-  #   -m 'What do you want to do?' \
-  #   -b 'Poweroff' 'systemctl poweroff' \
-  #   -b 'Reboot' 'systemctl reboot'
-  greetdHyprlandConfig = pkgs.writeText "greetd-hyprland-config" ''
-    exec-once = "${lib.getExe pkgs.greetd.regreet}; hyprctl dispatch exit"
-  '';
-in
+# bindsym XF86MonBrightnessUp exec light -A 5
+# bindsym XF86MonBrightnessDown exec light -U 5
+# bindsym Print exec ${lib.getExe pkgs.grim} /tmp/regreet.png
+# bindsym Mod4+shift+e exec swaynag \
+#   -t warning \
+#   -m 'What do you want to do?' \
+#   -b 'Poweroff' 'systemctl poweroff' \
+#   -b 'Reboot' 'systemctl reboot'
+#   greetdHyprlandConfig = pkgs.writeText "greetd-hyprland-config" ''
+#     exec-once = "${lib.getExe pkgs.greetd.regreet}; hyprctl dispatch exit"
+#   '';
+# in
+# services.greetd = {
+#   enable = true;
+#   settings.default_session = {
+#     command = "${config.programs.hyprland.package}/bin/Hyprland --config ${greetdHyprlandConfig}";
+#     user = "greeter";
+#   };
+# };
+# programs.regreet = {
+#   enable = true;
+#   settings = {
+#     background = {
+#       path = ../../../wallpapers/nix-black-4k.png;
+#       fit = "Cover";
+#     };
+#     # GTK = {
+#     #   cursor_theme_name = "Bibata-Modern-Classic";
+#     #   font_name = "Jost * 12";
+#     #   icon_theme_name = "Papirus-Dark";
+#     #   theme_name = "Catppuccin-Mocha-Compact-Mauve-Dark";
+#     # };
+#   };
+# };
 {
   programs.waybar.enable = true;
   programs.hyprland = {
     enable = true;
   };
-  # services.greetd = {
-  #   enable = true;
-  #   settings.default_session = {
-  #     command = "${config.programs.hyprland.package}/bin/Hyprland --config ${greetdHyprlandConfig}";
-  #     user = "greeter";
-  #   };
-  # };
   services.xserver.displayManager.gdm = {
     enable = true;
     wayland = true;
   };
-  # programs.regreet = {
-  #   enable = true;
-  #   settings = {
-  #     background = {
-  #       path = ../../../wallpapers/nix-black-4k.png;
-  #       fit = "Cover";
-  #     };
-  #     # GTK = {
-  #     #   cursor_theme_name = "Bibata-Modern-Classic";
-  #     #   font_name = "Jost * 12";
-  #     #   icon_theme_name = "Papirus-Dark";
-  #     #   theme_name = "Catppuccin-Mocha-Compact-Mauve-Dark";
-  #     # };
-  #   };
-  # };
   services.gnome.gnome-keyring.enable = true; # Store secrets securely (Wifi passwords,git tokens, etc...)
 
   environment.systemPackages = (with pkgs; [
@@ -58,10 +58,11 @@ in
     wofi # Runner
     swaybg # Wallpaper ultility
     hyprpicker # Color picker
-    wl-clipboard copyq # Clipboard
+    wl-clipboard
+    copyq # Clipboard
     libsForQt5.polkit-kde-agent # Graphical root elevation
     gparted # Test polkit
-    
+
     # Dolphin and assorted dependencies for it
     taglib
     ffmpegthumbnailer
@@ -71,7 +72,8 @@ in
     baloo
     dolphin-plugins
     kdegraphics-thumbnailers
-    kio kio-extras
+    kio
+    kio-extras
     breeze-icons
   ]);
 
