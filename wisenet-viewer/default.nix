6c045d96add60d3d7e8d4c30cfce3711f42e745f
benshewan <benbshewan@gmail.com>
2023-09-16 00:30:45 -0300

    holy shit it works

--- wisenet-viewer/default.nix
+++ wisenet-viewer/default.nix
@@ -2,6 +2,7 @@
 , fetchurl
 , unzip
 , dpkg
+, buildFHSUserEnv
 , ...
 }:
 
@@ -13,30 +14,54 @@ let
     url = "https://hanwhavisionamerica.com/download/48379";
     sha256 = "sha256-WTets2nGNFm7baNiBQq/2FW9N0s5jKRTFYTFet8LGKI=";
   };
+  wisenet-viewer-environment = stdenv.mkDerivation {
+    pname = "wisenet-viewer";
+    inherit src version;
+
+    nativeBuildInputs = [ dpkg unzip ];
+
+    unpackPhase = ''
+      unzip $src
+      dpkg-deb -x ./WisenetViewer_1.04.00_20230511.deb .
+    '';
+
+
+    installPhase = ''
+      mkdir -p $out/etc
+      mkdir -p $out/opt
+      mv usr/* $out
+      mv etc/* $out/etc
+      mv opt/* $out/opt
+
+      substituteInPlace $out/share/applications/wisenet-viewer.desktop \
+        --replace "/opt/HanwhaVision/WisenetViewer/WisenetViewer.sh" "$out/opt/HanwhaVision/WisenetViewer/WisenetViewer.sh" \
+
+      substituteInPlace $out/opt/HanwhaVision/WisenetViewer/WisenetViewer.sh \
+        --replace '$(dirname "$(readlink -f "$0")")' "$out/opt/HanwhaVision/WisenetViewer" \
+    '';
+  };
 in
-stdenv.mkDerivation {
-  pname = "wisenet-viewer";
-  inherit src version;
-
-  nativeBuildInputs = [ dpkg unzip ];
-
-  unpackPhase = ''
-    unzip $src
-    dpkg-deb -x ./WisenetViewer_1.04.00_20230511.deb .
-  '';
-
-
-  installPhase = ''
-    mkdir -p $out/etc
-    mkdir -p $out/opt
-    mv usr/* $out
-    mv etc/* $out/etc
-    mv opt/* $out/opt
-
-    substituteInPlace $out/share/applications/wisenet-viewer.desktop \
-      --replace "/opt/HanwhaVision/WisenetViewer/WisenetViewer.sh" "$out/opt/HanwhaVision/WisenetViewer/WisenetViewer.sh" \
-
-    substituteInPlace $out/opt/HanwhaVision/WisenetViewer/WisenetViewer.sh \
-      --replace '$(dirname "$(readlink -f "$0")")' "$out/opt/HanwhaVision/WisenetViewer" \
-  '';
-}
+(buildFHSUserEnv {
+  name = "WisenetViewer-fhs";
+  targetPkgs = p: with p; [
+    wisenet-viewer-environment
+    fontconfig
+    freetype
+    xorg.libX11
+    libusb1.out
+    xorg.xcbutilwm
+    xorg.xcbutilimage
+    xorg.xcbutil
+    xorg.xcbutilkeysyms
+    xorg.xcbutilrenderutil
+    libxkbcommon
+    libpng
+    libGL
+    libpulseaudio
+    glib
+    zlib
+    krb5
+    xorg.libxcb
+  ];
+  runScript = "/opt/HanwhaVision/WisenetViewer/WisenetViewer.sh";
+})
