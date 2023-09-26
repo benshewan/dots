e85250c2c7de2ae83f832407146def5d9bfdf85e
benshewan <benbshewan@gmail.com>
2023-09-26 11:40:01 -0300

    added network shares to work pc

--- systems/lepus/default.nix
+++ systems/lepus/default.nix
@@ -7,6 +7,7 @@
 }: {
   imports = [
     ./hardware-configuration.nix
+    ./networking.nix
     ../shared
     ../shared/desktop-enviroments/gnome.nix
     inputs.nixos-hardware.nixosModules.common-pc
