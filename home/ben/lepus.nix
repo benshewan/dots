7c02f32bb89982ad9fca288ae86f9beab6291109
Ben Shewan <benbshewan@gmail.com>
2023-10-23 09:18:24 -0300

    changed lepus gtk config

--- home/ben/lepus.nix
+++ home/ben/lepus.nix
@@ -1,7 +1,24 @@
-{outputs, ...}: {
+{
+  outputs,
+  lib,
+  pkgs,
+  ...
+}: {
   imports = [./global ../shared/desktop-enviroments/gnome.nix];
 
   home.shellAliases = {
     home-switch = "home-manager switch --flake ${outputs.flake-path}#ben@lepus";
+  };
+
+  gtk.theme = lib.mkForce {
+    name = "Catppuccin-Mocha-Standard-Blue-dark";
+    package =
+      pkgs.catppuccin-gtk.override
+      {
+        accents = ["blue"];
+        size = "standard"; # compact
+        tweaks = [];
+        variant = "mocha";
+      };
   };
 }
