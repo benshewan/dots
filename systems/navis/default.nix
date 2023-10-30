026700c5ccd50d0912d74c99dfa07b282116aa6b
Ben Shewan <benbshewan@gmail.com>
2023-10-30 10:25:26 -0300

    updated navis to use WIP nixos hardware config

--- systems/navis/default.nix
+++ systems/navis/default.nix
@@ -1,26 +1,55 @@
 {
   inputs,
   pkgs,
+  lib,
   ...
 }: {
   imports = [
     ./hardware-configuration.nix
     ../shared
     ../shared/desktop-enviroments/hyprland.nix
-    # inputs.nixos-hardware.nixosModules.framework
     inputs.nixos-hardware.nixosModules.common-cpu-amd
     inputs.nixos-hardware.nixosModules.common-cpu-amd-pstate
     inputs.nixos-hardware.nixosModules.common-gpu-amd
-    # inputs.nixos-hardware.nixosModules.common-pc-laptop
+    inputs.nixos-hardware.nixosModules.common-pc-laptop
     inputs.nixos-hardware.nixosModules.common-pc-laptop-ssd
   ];
 
-  boot.kernelParams = [
-    # For Power consumption
-    # https://kvark.github.io/linux/framework/2021/10/17/framework-nixos.html
-    # "mem_sleep_default=deep"
-    "acpi_osi=\"!Windows 2020\""
-  ];
+  # Newer kernel is better for amdgpu driver updates
+  # Requires at least 5.16 for working wi-fi and bluetooth (RZ616, kmod mt7922):
+  # https://wireless.wiki.kernel.org/en/users/drivers/mediatek
+  boot.kernelPackages = lib.mkIf (lib.versionOlder pkgs.linux.version "6.1") (lib.mkDefault pkgs.linuxPackages_latest);
+
+  # AMD has better battery life with PPD over TLP:
+  # https://community.frame.work/t/responded-amd-7040-sleep-states/38101/13
+  services.power-profiles-daemon.enable = lib.mkDefault true;
+
+  # Fix TRRS headphones missing a mic
+  # https://community.frame.work/t/headset-microphone-on-linux/12387/3
+  boot.extraModprobeConfig = ''
+    options snd-hda-intel model=dell-headset-multi
+
+    options cfg80211 ieee80211_regdom="CA"
+  '';
+
+  # For fingerprint support
+  services.fprintd.enable = lib.mkDefault true;
+
+  # Custom udev rules
+  services.udev.extraRules = ''
+    # Ethernet expansion card support
+    ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="0bda", ATTR{idProduct}=="8156", ATTR{power/autosuspend}="20"
+  '';
+
+  # Needed for desktop environments to detect/manage display brightness
+  hardware.sensor.iio.enable = lib.mkDefault true;
+
+  # boot.kernelParams = [
+  #   # For Power consumption
+  #   # https://kvark.github.io/linux/framework/2021/10/17/framework-nixos.html
+  #   # "mem_sleep_default=deep"
+  #   "acpi_osi=\"!Windows 2020\""
+  # ];
 
   # System
   networking.hostName = "navis";
@@ -31,37 +60,14 @@
   # Auto Brightness
   location.provider = "geoclue2";
   services.clight = {
-    enable = true;
+    enable = false;
     settings = {
       # dimmer.disabled = false;
     };
   };
 
-  # Power management
-  services.power-profiles-daemon.enable = true;
-  # services.tlp.enable = true;
-  # services.auto-cpufreq = {
-  #   enable = true;
-  #   settings = {
-  #     charger = {
-  #       governor = "preformance";
-  #       scaling_min_freq = 3300000; # 800MHz
-  #       scaling_max_freq = 5100000; # 1GHz
-  #       turbo = "auto";
-  #     };
-  #     battery = {
-  #       governor = "powersave";
-  #       scaling_min_freq = 800000; # 800MHz
-  #       scaling_max_freq = 3300000; # 1GHz
-  #       turbo = "auto";
-  #     };
-  #   };
-  # };
   # Maybe needed for Thunderbolt - not sure yet
-  services.hardware.bolt.enable = true;
-
-  # Fingerprint
-  services.fprintd.enable = true;
+  # services.hardware.bolt.enable = true;
 
   # Firmware
   services.fwupd = {
@@ -71,9 +77,9 @@
 
   # Need to set regulatory domain for AMD RZ616 wifi card
   hardware.wirelessRegulatoryDatabase = true;
-  boot.extraModprobeConfig = ''
-    options cfg80211 ieee80211_regdom="CA"
-  '';
+  # boot.extraModprobeConfig = ''
+  #   options cfg80211 ieee80211_regdom="CA"
+  # '';
 
   environment.systemPackages = with pkgs; [
     # Audio Configuration https://github.com/ceiphr/ee-framework-presets
@@ -81,5 +87,7 @@
 
     # View Power Draw
     powertop
+
+    nm-tray
   ];
 }
