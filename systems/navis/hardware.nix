955e8a180a46fe3050557a1cb9932ebaee6572aa
Ben Shewan <benbshewan@gmail.com>
2023-11-20 19:18:40 -0400

    added udev rules to control ppd profiles

--- systems/navis/hardware.nix
+++ systems/navis/hardware.nix
@@ -8,6 +8,17 @@
 
   # Additional power savings
   powerManagement.powertop.enable = true;
+
+  # Switch Power Profiles based on if plugged in or not
+  # maybe
+  # battery - ENV{POWER_SUPPLY_ONLINE}=="0"
+  # AC - ENV{POWER_SUPPLY_ONLINE}=="1"
+  services.udev.extraRules = ''
+    SUBSYSTEM=="power_supply",ATTR{online}=="1",RUN+="${pkgs.power-profiles-daemon}/bin/powerprofilesctl set performance"
+    SUBSYSTEM=="power_supply",ATTR{status}=="Discharging",ATTR{capacity_level}=="Normal",RUN+="${pkgs.power-profiles-daemon}/bin/powerprofilesctl set power-saver"
+    SUBSYSTEM=="power_supply",ATTR{status}=="Discharging", ATTR{capacity_level}=="Low",RUN+="${pkgs.power-profiles-daemon}/bin/powerprofilesctl set power-saver"
+  '';
+
   services.auto-cpufreq.enable = true;
   services.auto-cpufreq.settings = {
     battery = {
@@ -21,17 +32,17 @@
   };
 
   # AMD OpenGL/Vulkan stuff
-  hardware.opengl.extraPackages = [pkgs.rocm-opencl-icd pkgs.amdvlk];
+  # hardware.opengl.extraPackages = [pkgs.rocm-opencl-icd pkgs.amdvlk];
 
   boot.kernelParams = [
     # Potential fix for video stuttering
     # "amd_iommu=off"
     # reported to help with flashing display issues
-    "amdgpu.sg_display=0"
+    # "amdgpu.sg_display=0"
   ];
 
   # Add support for temp, voltage, current, and power reading
-  boot.extraModulePackages = with config.boot.kernelPackages; [zenpower];
+  # boot.extraModulePackages = with config.boot.kernelPackages; [zenpower];
 
   # Auto Brightness
   location.provider = "geoclue2";
@@ -49,8 +60,8 @@
   };
 
   # Need to set regulatory domain for AMD RZ616 wifi card
-  # hardware.wirelessRegulatoryDatabase = true;
-  # boot.extraModprobeConfig = ''
-  #   options cfg80211 ieee80211_regdom="CA"
-  # '';
+  hardware.wirelessRegulatoryDatabase = true;
+  boot.extraModprobeConfig = ''
+    options cfg80211 ieee80211_regdom="CA"
+  '';
 }
