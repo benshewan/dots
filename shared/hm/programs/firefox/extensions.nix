48dbe43ce35071af446b2c43a9e67a2ec1369288
Ben Shewan <benbshewan@gmail.com>
2024-01-21 21:14:54 -0400

    firefox extensions can now be disabled

--- shared/hm/programs/firefox/extensions.nix
+++ shared/hm/programs/firefox/extensions.nix
@@ -22,19 +22,19 @@ in {
 
   # Ad Blocker
   "uBlock0@raymondhill.net" = {
-    installation_mode = "force_installed";
+    installation_mode = "normal_installed";
     install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
   };
 
   # Blocks Sponsor segments on Youtube videos
   "sponsorBlocker@ajay.app" = {
-    installation_mode = "force_installed";
+    installation_mode = "normal_installed";
     install_url = "https://addons.mozilla.org/firefox/downloads/latest/sponsorblock/latest.xpi";
   };
 
   # Bypass Website Paywalls
   "magnolia@12.34" = {
-    installation_mode = "force_installed";
+    installation_mode = "normal_installed";
     install_url = "https://gitlab.com/magnolia1234/bpc-uploads/-/raw/master/bypass_paywalls_clean-latest.xpi";
   };
 
@@ -43,14 +43,14 @@ in {
 
   # Torrent Control
   "{e6e36c9a-8323-446c-b720-a176017e38ff}" = {
-    installation_mode = "force_installed";
+    installation_mode = "normal_installed";
     default_area = "menupanel";
     install_url = "https://addons.mozilla.org/firefox/downloads/latest/torrent-control/latest.xpi";
   };
 
   # NZB Downloader
   "{96586e48-b9a2-45dd-b1a1-54fa85a97c91}" = {
-    installation_mode = "force_installed";
+    installation_mode = "normal_installed";
     install_url = "https://addons.mozilla.org/firefox/downloads/latest/nzb-unity/latest.xpi";
   };
 
@@ -59,18 +59,18 @@ in {
 
   # Integration with KDE / Hyprland / GNOME
   # "gsconnect@andyholmes.github.io" = lib.optionalAttrs (lib.elem pkgs.gnomeExtensions.gsconnect config.home.packages) {
-  #   installation_mode = "force_installed";
+  #   installation_mode = "normal_installed";
   #   install_url = "https://addons.mozilla.org/firefox/downloads/latest/gsconnect/latest.xpi";
   # };
 
   "plasma-browser-integration@kde.org" = {
-    installation_mode = "force_installed";
+    installation_mode = "normal_installed";
     install_url = "https://addons.mozilla.org/firefox/downloads/latest/plasma-integration/latest.xpi";
   };
 
   # Gnome Shell Integration
   # "chrome-gnome-shell@gnome.org" = lib.optionalAttrs (lib.elem pkgs.gnomeExtensions.gsconnect config.home.packages) {
-  #   installation_mode = "force_installed";
+  #   installation_mode = "normal_installed";
   #   install_url = "https://addons.mozilla.org/firefox/downloads/latest/gnome-shell-integration/latest.xpi";
   # };
 
@@ -79,20 +79,20 @@ in {
 
   # Sidebery Tabs
   "{3c078156-979c-498b-8990-85f7987dd929}" = {
-    installation_mode = "force_installed";
+    installation_mode = "normal_installed";
     # default_area = "navbar";
     install_url = "https://addons.mozilla.org/firefox/downloads/latest/sidebery/latest.xpi";
   };
 
   # New Tab Home Page
   "extension@tabliss.io" = {
-    installation_mode = "force_installed";
+    installation_mode = "normal_installed";
     install_url = "https://addons.mozilla.org/firefox/downloads/latest/tabliss/latest.xpi";
   };
 
   # Multi-Account Containers
   "@testpilot-containers" = {
-    installation_mode = "force_installed";
+    installation_mode = "normal_installed";
     install_url = "https://addons.mozilla.org/firefox/downloads/latest/multi-account-containers/latest.xpi";
   };
 
@@ -101,37 +101,37 @@ in {
 
   # Youtube customization
   # "enhancerforyoutube@maximerf.addons.mozilla.org" = {
-  #   installation_mode = "force_installed";
+  #   installation_mode = "normal_installed";
   #   install_url = "https://addons.mozilla.org/firefox/downloads/latest/enhancer-for-youtube/latest.xpi";
   # };
 
   # Remove YouTube Shorts
   "{88ebde3a-4581-4c6b-8019-2a05a9e3e938}" = {
-    installation_mode = "force_installed";
+    installation_mode = "normal_installed";
     install_url = "https://addons.mozilla.org/firefox/downloads/latest/hide-youtube-shorts/latest.xpi";
   };
 
   # Youtube customization
   "enhancerforyoutube@maximerf.addons.mozilla.org" = {
-    installation_mode = "force_installed";
+    installation_mode = "normal_installed";
     install_url = "file://${./enhancer_for_youtube-2.0.121.xpi}";
   };
 
   # View Xpi Id's in Firefox Extension Store
   "queryamoid@kaply.com" = {
-    installation_mode = "force_installed";
+    installation_mode = "normal_installed";
     install_url = "https://github.com/mkaply/queryamoid/releases/download/v0.1/query_amo_addon_id-0.1-fx.xpi";
   };
 
   # Reddit
   "jid1-xUfzOsOFlzSOXg@jetpack" = {
-    installation_mode = "force_installed";
+    installation_mode = "normal_installed";
     install_url = "https://addons.mozilla.org/firefox/downloads/latest/reddit-enhancement-suite/latest.xpi";
   };
 
   # Twitch
   "twitch5@coolcmd" = {
-    installation_mode = "force_installed";
+    installation_mode = "normal_installed";
     install_url = "https://addons.mozilla.org/firefox/downloads/latest/twitch_5/latest.xpi";
   };
   # Development
@@ -139,13 +139,13 @@ in {
 
   # React Integration
   "@react-devtools" = {
-    installation_mode = "force_installed";
+    installation_mode = "normal_installed";
     install_url = "https://addons.mozilla.org/firefox/downloads/latest/react-devtools/latest.xpi";
   };
 
   # Redux Integration
   "extension@redux.devtools" = {
-    installation_mode = "force_installed";
+    installation_mode = "normal_installed";
     install_url = "https://addons.mozilla.org/firefox/downloads/latest/reduxdevtools/latest.xpi";
   };
 
@@ -154,14 +154,14 @@ in {
 
   # Password Manager
   "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
-    installation_mode = "force_installed";
+    installation_mode = "normal_installed";
     default_area = "navbar";
     install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
   };
 
   # View the web in dark mode
   "addon@darkreader.org" = {
-    installation_mode = "force_installed";
+    installation_mode = "normal_installed";
     install_url = "https://addons.mozilla.org/firefox/downloads/latest/darkreader/latest.xpi";
   };
 
@@ -170,13 +170,13 @@ in {
 
   # Allow you to hold ctrl to click substring of url
   "advancedlocationbar@veg.by" = {
-    installation_mode = "force_installed";
+    installation_mode = "normal_installed";
     install_url = "file://${./AdvancedLocationbar2_1.2.1.7.xpi}";
   };
 
   # New tabs inhert the history of their parent tabs
   "backtrack@byalexv.co.uk" = {
-    installation_mode = "force_installed";
+    installation_mode = "normal_installed";
     install_url = "file://${legacy-extensions}/extensions/backtrack/BackTrack.xpi";
   };
   # Tab Mix Plus
