42ba206fca143283fd384266a844aa1eb924549e
Ben Shewan <benbshewan@gmail.com>
2024-12-17 20:33:20 -0400

    fixed home manager import

--- modules/darwin/system/default.nix
+++ modules/darwin/system/default.nix
@@ -10,7 +10,6 @@
 {
 
   imports = [inputs.mac-app-util.darwinModules.default];
-  ${namespace}.home.extraOptions.imports = [inputs.mac-app-util.homeManagerModules.default];
   system = {
     stateVersion = 5;
     # activationScripts are executed every time you boot the system or run `nixos-rebuild` / `darwin-rebuild`.
--- modules/home/home/default.nix
+++ modules/home/home/default.nix
@@ -1,8 +1,14 @@
 {
   lib,
+  pkgs,
+  inputs,
   osConfig ? {},
   ...
-}: {
+}: let
+  inherit (pkgs.stdenv) isDarwin;
+in {
+  imports = [] ++ lib.optional isDarwin inputs.mac-app-util.homeManagerModules.default;
+
   home.stateVersion = lib.mkDefault (lib.mkIf (osConfig != {} && osConfig.system.stateVersion) osConfig.system.stateVersion // "23.11");
   # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
