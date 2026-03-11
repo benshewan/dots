3d48beeecc8576ef9b54f65b94e444760d90ba8b
Ben Shewan <benbshewan@gmail.com>
2026-03-11 14:02:38 -0300

    switched npm to home

--- dendritic/modules/programs/npm.nix
+++ dendritic/modules/programs/npm.nix
@@ -1,21 +1,21 @@
 _: {
-  flake.modules.nixos."programs/npm" = {pkgs, ...}: {
-    environment.sessionVariables.NODE_REPL_HISTORY = ''$XDG_CACHE_HOME/node_repl_history'';
+  flake.modules.homeManager."programs/npm" = {pkgs, ...}: {
+    home.sessionVariables.NODE_REPL_HISTORY = ''$XDG_CACHE_HOME/node_repl_history'';
 
-    environment.systemPackages = with pkgs; [
+    home.packages = with pkgs; [
       nodejs
     ];
 
     programs.npm = {
       enable = true;
-      npmrc = ''
-        prefix=''${XDG_DATA_HOME}/npm
-        cache=''${XDG_CACHE_HOME}/npm
-        init-module=''${XDG_CONFIG_HOME}/npm/config/npm-init.js
-        logs-dir=''${XDG_STATE_HOME}/npm/logs
-        color = true
-        fund = false
-      '';
+      settings = {
+        prefix = ''''${XDG_DATA_HOME}/npm'';
+        cache = ''''${XDG_CACHE_HOME}/npm'';
+        init-module = ''''${XDG_CONFIG_HOME}/npm/config/npm-init.js'';
+        logs-dir = ''''${XDG_STATE_HOME}/npm/logs'';
+        color = true;
+        fund = false;
+      };
     };
   };
 }
