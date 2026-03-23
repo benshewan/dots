1acd6354cd4be32831024a3af0d18806133503cb
Ben Shewan <benbshewan@gmail.com>
2026-03-22 22:38:58 -0300

    added hyprland cache

--- modules/window-managers/hyprland/hyprland.nix
+++ modules/window-managers/hyprland/hyprland.nix
@@ -11,6 +11,13 @@
   }: let
     inherit (pkgs.stdenv.hostPlatform) system;
   in {
+    # Hyprland Cache
+    nix.settings = {
+      substituters = ["https://hyprland.cachix.org"];
+      trusted-substituters = ["https://hyprland.cachix.org"];
+      trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+    };
+
     programs.uwsm.enable = true;
     programs.dconf.enable = true;
     programs.hyprland = {
